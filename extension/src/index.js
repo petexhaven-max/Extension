f633b60af7dac4b2c275ed713e5ae3447be406f4
petexhaven-max <petexhaven@gmail.com>
2026-02-08 23:21:16 +0600

    Update index.js

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-  <title>SuperNewsFeed</title>
-  <link rel="stylesheet" href="src/styles.css" />
-</head>
-<body>
-  <div id="root"></div>
-  <script src="src/index.js"></script>
-</body>
-</html>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+import "./styles.css";
+
+createRoot(document.getElementById("root")).render(<App />);

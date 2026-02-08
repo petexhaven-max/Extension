c4efd167a97c4f4ea026cd2c103ecfc8b188b7ad
petexhaven-max <petexhaven@gmail.com>
2026-02-08 23:16:08 +0600

    Add HTML template to index.js
    Added basic HTML structure for the extension.

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <title>SuperNewsFeed</title>
+  <link rel="stylesheet" href="src/styles.css" />
+</head>
+<body>
+  <div id="root"></div>
+  <script src="src/index.js"></script>
+</body>
+</html>
